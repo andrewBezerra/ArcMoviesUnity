a091b01ec6d0cf478a8d63fadd98de235ca34109
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2019-11-30 13:25:28 -0300

    MUDANÇA APIKEY

--- ArcMoviesUnity/ArcMoviesUnity/Helpers/AppSettings.cs
+++ ArcMoviesUnity/ArcMoviesUnity/Helpers/AppSettings.cs
@@ -8,7 +8,7 @@ namespace ArcMoviesUnity.Helpers
         public static string PosterImageBaseUrl = "http://image.tmdb.org/t/p/w185/";
         public static string BackdropImageBaseUrl = "http://image.tmdb.org/t/p/w500/";
         public static string ApiBaseUrl = "https://api.themoviedb.org/3/";
-        public static string ApiKey = "SUA KEY AQUI";
+        public static string ApiKey = APIKEY.Secret; // REMOVA ISSO E COLOQUE SUA KEY AQUI
         
     }
 }
